bb1cd0f66d4cfb62cb2796e71f094950b0040dee
Siddhant Agarwal <agarwalsiddhant10@gmail.com>
2022-02-28 02:52:10 +0530

    Add multiprocessing

--- triplet_prediction.py
+++ triplet_prediction.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from multiprocessing import Pool, Manager
 from constants import *
 from embedding_helper import *
 from data_handler import *
@@ -13,25 +14,35 @@ def create_entity_index2id(entity_dict):
 def triple_score(e, r, o):
   return np.linalg.norm(e + r - o, ord=2)
 
-def get_predictions_sorted(entity_vec, entity_dict, relation, occupation):
+def update_scores_dict(scores, entityindex2id, index, e, r, o):
+    score = triple_score(e, r, o)
+    #entity index -> entity id
+    entity_id = entityindex2id[index]
+    #store <score, entity id>
+    scores[entity_id] = score
+
+def get_predictions_sorted(entity_vec, entityindex2id, relation, occupation):
     #get occupation vector
     occuptation_vector = get_averaged_entity_vector(entity_vec, entity_dict, DIMS, occupation)
-    scores = {}
-    for index, entity in enumerate(entity_vec):
-        score = triple_score(entity, relation, occupation)
-        #entity index -> entity id
-        entity_id = entity
-        #store <score, entity id>
-        score[entity_id] = score
+    manager = Manager()
+    scores = manager.dict()
+    with Pool(8) as p:
+        p.imap_unordered(update_score, [scores, entityindex2id, i, e, relation, occupation_vector for i, e in enumerate(entity_vec)] )
+    # for index, entity in enumerate(entity_vec):
+    #     score = triple_score(entity, relation, occupation)
+    #     #entity index -> entity id
+    #     entity_id = entityindex2id[index]
+    #     #store <score, entity id>
+    #     score[entity_id] = score
 
     #sort and return
     return dict(sorted(scores.items(), key=lambda item : item[0]))
 
-def top_x_gender(triples, entity_vec, entity_labels, relations_vec, relations_dict, entity_dict, occupation, X=10):
+def top_x_gender(triples, entity_vec, entity_labels, relations_vec, relations_dict, entityindex2id, occupation, X=10):
     #relation vector
     relation_vector = relations_vec[relations_dict[occupation_relation_id]]
     #get sorted list
-    sorted_scores = get_predictions_sorted(entity_vec, entity_dict, relation_vector, occupation)
+    sorted_scores = get_predictions_sorted(entity_vec, entityindex2id, relation_vector, occupation)
     #top x
     top_x_scores = dict(sorted_scores.items()[:X])
     #for each entity
@@ -49,7 +60,7 @@ def top_x_gender(triples, entity_vec, entity_labels, relations_vec, relations_di
 
 def top_x_gender_gt(triples, occupation, X=10):
     #list of people in occupation
-    people_in_occupation = triples[(triples["tail"]==occupation) & (triples["relation"]==occupation_relation_name)]["name"]
+    people_in_occupation = triples[(triples["tail"]==occupation) & (triples["relation"]==occupation_relation_name)]["head"]
 
     male_counts = 0
     female_counts = 0
